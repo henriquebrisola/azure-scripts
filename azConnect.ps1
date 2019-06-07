7d7acd5f4b44db46ab2167e4c50c49ca31143d60
Henrique <hbrisola@equinix.com>
2019-06-07 11:03:01 -0300

    fixing path of OS to work on both ISE and PScmd

--- azConnect.ps1
+++ azConnect.ps1
@@ -1,8 +1,15 @@
+#fix os location
+if ($psISE)
+{
+    $scriptPath = Split-Path -Path $psISE.CurrentFile.FullPath
+}
+else
+{
+    $scriptPath = $PSScriptRoot
+}
+
 #connect to Azure
 function azConnect{
-    $contextFile = "azContext.json"
-    $location = $psISE.CurrentFile.FullPath -replace $psISE.CurrentFile.DisplayName, $contextFile
-    $PSScriptRoot # This is an automatic variable set to the current file's/module's directory
-
-    Import-AzureRmContext -path $location.ToString()
+    $contextFile = $scriptPath + ".\azContext.json"
+    Import-AzureRmContext -path $contextFile #insert full path
 }
