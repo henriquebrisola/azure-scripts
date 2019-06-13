50eac4b4aa98019a6f2cae228ede192360f4ef02
Henrique <henriquebrisola@gmail.com>
2019-06-13 14:03:25 -0300

    added option to lsit or not VMs, improving performance

--- azPlayVM.ps1
+++ azPlayVM.ps1
@@ -14,41 +14,68 @@ else
     . .\azStartStopRestart.ps1
     . .\azViewVMs.ps1
 
-#connect
-    if ($connected) {
-    }
-    else{    #runs once per session
-        azConnect
-        $connected = $true
-    }
-
-#select
-    azViewVMs #lists VMs
-        
 #parameters
-    $vmList = @()
-    $storageSwitchYN = @()
-    $runOpt = Read-Host "Start, Stop or Restart?"
-    Do{
-        $vmName = Read-Host "Type the name of the VM or 'n' for No"
-        if ($vmName -ne "n"){
-            $vmList += Get-AzureRmVM -Name $vmName -ResourceGroupName $vmName -status
-            $storageSwitchYN += Read-Host "Switch the Storage Option? [y,n]"
+    function script:parameters(){
+        $script:vmList = @()
+        $script:storageSwitchYN = @()
+        $script:runOpt = Read-Host "Start, Stop or Restart?"
+        Do{
+            $vmName = Read-Host "Type the name of the VM or 'n' for No"
+            if ($vmName.ToLower() -ne "n"){
+                $script:vmList += $vmName
+                $script:storageSwitchYN += Read-Host "Switch the Storage Option? [y,n]"
+            }
         }
+        Until ($vmName.ToLower() -eq "n")
     }
-    Until ($vmName -eq "n")
 
 #storageType = Premium_LRS, StandardSSD_LRS, Standard_LRS
-    If ($runOpt -eq "start") {
-        $storageType = "Premium_LRS"  
-    }
-    ElseIf ($runOpt -eq "stop"){
-        $storageType = "Standard_LRS"
+    function storageType(){
+        If ($runOpt.ToLower() -eq "start") {
+            $script:storageType = "Premium_LRS"  
+        }
+        ElseIf ($runOpt.ToLower() -eq "stop"){
+            $script:storageType = "Standard_LRS"
+        }
     }
 
 #run
-    $i = 0
-    ForEach ($vm in $vmList){
-        azRunVM $runOpt $storageSwitchYN[$i] $storageType $vm[$i]
-        $i = $i + 1
+    function run(){
+        $i = 0
+        ForEach ($vmName in $vmList){
+            $vm = Get-AzureRmVM -Name $vmName -ResourceGroupName $vmName -status
+            azRunVM $runOpt $storageSwitchYN[$i] $storageType $vm
+            $i = $i + 1
+        }
+    }
+
+#execution
+    $ListVMsOpt = Read-Host "Do you want to list all VMs? [y/n]"
+    if ($ListVMsOpt.ToLower() -eq "y") {
+        #connect
+            if ($connected) {
+            }
+            else{    #runs once per session
+                azConnect
+                $connected = $true
+            }
+        azViewVMs
+        parameters
+        storageType
+        run
+    }
+    elseif ($ListVMsOpt.ToLower() -eq "n") {
+        parameters
+        storageType
+        #connect
+            if ($connected) {
+            }
+            else{    #runs once per session
+                azConnect
+                $connected = $true
+            }
+        run
+    }
+    else {
+        Write-Host "wrong option"
     }
